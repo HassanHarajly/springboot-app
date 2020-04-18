7c40269b576c68d52bdc105fa25241730f98b06e
unknown <hassanmharajly@hotmail.com>
2020-04-17 21:48:29 -0400

    hharajl1 added logger

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -2,7 +2,9 @@ package com.example.demo.api;
 
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
-import org.springframework.beans.factory.annotation.Autowired;
+import javafx.application.Application;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonController {
     private final PersonService personService;
-
+    private static final Logger LOGGER=LoggerFactory.getLogger(Application.class);
+    Logger logger = LoggerFactory.getLogger("ol");
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
@@ -19,5 +22,6 @@ public class PersonController {
     public void addPerson(@RequestBody Person person)
     {
         personService.addPerson(person);
+        LOGGER.error("Simple log statement with inputs {}, {} and {}", 1,2,3);
     }
 }
