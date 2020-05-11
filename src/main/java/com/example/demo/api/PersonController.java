0abbffbf6eeebba9638b430e264c945d903bbbb6
unknown <hassanmharajly@hotmail.com>
2020-05-11 18:32:37 -0400

    hharajl1 downgrading to java 8 because pcf only supports 8

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -2,27 +2,26 @@ package com.example.demo.api;
 
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
-import javafx.application.Application;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @RequestMapping("/api/v1/person")
 @RestController
 public class PersonController {
     private final PersonService personService;
-    private static final Logger LOGGER=LoggerFactory.getLogger(Application.class);
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
     @PostMapping
     public void addPerson(@RequestBody Person person)
     {
-
-        LOGGER.error(person.getName().toString());
         personService.addPerson(person);
-        LOGGER.error("Simple log statement with inputs {}, {} and {}", 1,2,3);
+    }
+    @GetMapping("/getMapping")
+    public int returnNum()
+    {
+       return 1;
     }
 }
