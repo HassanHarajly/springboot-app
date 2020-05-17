4723bc43b194ab067e79ca6be5749ea6c4b20f7f
unknown <hassanmharajly@hotmail.com>
2020-05-16 22:09:56 -0400

    hharajl1 pushing updated post and get mapping with comments to describe whats happening

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -2,26 +2,36 @@ package com.example.demo.api;
 
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/api/v1/person")
 @RestController
 public class PersonController {
+
     private final PersonService personService;
+
+
+    //no qualifier is needed here for the service since it will be treated as a singleton and as a result has no interface.
+    @Autowired
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
+
+    //postmapping tells the rest controller what function to call when a post request is made
+    //RequestBody tells spring where to put the post body data into
     @PostMapping
     public void addPerson(@RequestBody Person person)
     {
+
         personService.addPerson(person);
     }
-    @GetMapping("/getMapping")
-    public int returnNum()
+
+    @GetMapping
+   public List<Person> getAllPeople()
     {
-       return 1;
+        return personService.getAllPeople();
     }
 }
--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -1,23 +1,26 @@
 package com.example.demo.dao;
 import com.example.demo.model.Person;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+//this tells spring that this class needs to be instantiated as a bean. so that it can be injected into other classes
+//this class implements methods from the PersonDao Interface.
 @Repository("FakeDao")
 public class FakePersonDataAccessService implements  PersonDao {
+
     private static List<Person> DB= new ArrayList<>();
+
     @Override
     public int insertPerson(UUID id, Person person) {
         DB.add(new Person(id, person.getName()));
         return 1;
     }
 
+
     @Override
-    public int insertPerson(Person person) {
-        return 0;
+    public List<Person> selectAllPeople() {
+        return DB;
     }
 }
