f76d42ea6c24bbdacd647b22205836ad016786b4
unknown <hassanmharajly@hotmail.com>
2020-05-30 22:50:10 -0400

    hharajl1 connecting to aws rds db via jdbc and mysql

--- src/main/java/com/example/demo/dao/PersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -1,15 +1,24 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 //this is utilizing dependency injection to change the implementation in person service.
 //the other version of this dependency is in FakePersonDataAccessSerivce under "FakeDao"
-@Repository("postgres")
+@Repository("mysql")
 public class PersonDataAccessService implements PersonDao{
+    //autowired instantiates this bean and injects our datasource
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Override
     public int insertPerson(UUID id, Person person) {
         return 0;
@@ -17,7 +26,17 @@ public class PersonDataAccessService implements PersonDao{
 
     @Override
     public List<Person> selectAllPeople() {
-        return List.of(new Person(UUID.randomUUID(),"FROM POSTGRES DB"));
+        final String selectpeopleSql = "SELECT id, name FROM people";
+      List<Person> personList =   jdbcTemplate.query(selectpeopleSql, new RowMapper<Person>() {
+            @Override
+            public Person mapRow(ResultSet resultSet, int i) throws SQLException {
+                Person person = new Person( UUID.randomUUID(),resultSet.getString("name"));
+
+
+                return person;
+            }
+        });
+        return personList;
     }
 
     @Override
