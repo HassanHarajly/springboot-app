1d4e6aeeb6cf95f4adf3eb8c7c7888cffa412484
unknown <hassanmharajly@hotmail.com>
2020-06-07 14:17:24 -0400

    hharajl1 adding a testing controller to check if new api works in a new package

--- src/main/java/com/example/demo/barcodelookup/api/BarcodeController.java
+++ src/main/java/com/example/demo/barcodelookup/api/BarcodeController.java
@@ -1,4 +1,18 @@
 package com.example.demo.barcodelookup.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/itemLookup")
+@RestController
 public class BarcodeController {
+
+
+    @GetMapping(path = "{id}")
+    public String returnFunction(@PathVariable("id")int test)
+    {
+        return Integer.toString(test);
+    }
 }
