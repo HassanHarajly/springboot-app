3d642fe5d4b2096febd3472c7bcfdf8c25a79f95
unknown <hassanmharajly@hotmail.com>
2020-06-13 22:08:32 -0400

    hharajl1 renamed itemdata to product

--- src/main/java/com/example/demo/barcodelookup/dao/AwsBarcodeDao.java
+++ src/main/java/com/example/demo/barcodelookup/dao/AwsBarcodeDao.java
@@ -1,13 +1,19 @@
 package com.example.demo.barcodelookup.dao;
 
+import com.example.demo.barcodelookup.model.Product;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
 
 @Repository("MySql")
 
 public class AwsBarcodeDao implements ItemLookupDao {
 
+    private ArrayList<Product> products = new ArrayList<>();
     public String queryForBarCode()
     {
+
+        products.add(new Product());
         return "awsmysql";
     }
 
