a572275bdec766877db31f67061f90817df8b071
unknown <hassanmharajly@hotmail.com>
2021-01-03 11:48:08 -0500

    added full text search endpoint

--- src/main/java/com/example/demo/shopregistration/dao/repositories/ProductRepository.java
+++ src/main/java/com/example/demo/shopregistration/dao/repositories/ProductRepository.java
@@ -14,8 +14,9 @@ public interface ProductRepository extends CrudRepository<InStoreProduct,Integer
                     "SELECT top 20 *,   distance = GEOGRAPHY\\:\\:Point(:user_latitude, :user_longitude, 4326).STDistance(GEOGRAPHY\\:\\:Point(latitude, longitude, 4326)) / 1609.344 from products ORDER BY distance ASC",
             nativeQuery = true)
     List<InStoreProduct> getProximalProduct(@Param("user_latitude") Double userlatitude,@Param("user_longitude") Double userlongitude);
+// todo this logic works on keywords such as "computer water" should implement some logic that if this query returns nothing to try and split it by spaces and make additional queries to get as close as possible result.
+    @Query( value = "DECLARE @TestVariable AS VARCHAR(400) SET @TestVariable = 'computer water' SET @TestVariable = CONCAT('\"',@TestVariable,'\"') SELECT top 20 * FROM products  WHERE CONTAINS(product_name,@TestVariable)"
 
-    @Query( value = "SELECT top 20 * FROM products  WHERE CONTAINS(product_name,:product_name)"
     , nativeQuery = true)
     List<InStoreProduct> findByName(@Param("product_name") String product_name);
 }
